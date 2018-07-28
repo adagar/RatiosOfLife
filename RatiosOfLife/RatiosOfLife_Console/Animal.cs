58757ece5fa455a1371b04ef1c8b4029d07ce9f2
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-07-28 10:35:59 -0400

    Adding some inheritance

---
+++ RatiosOfLife/RatiosOfLife_Console/Animal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatiosOfLife_Console
+{
+    class Animal
+    {
+        public string gender { get; set; }
+        public Guid id { get; set; }
+        
+        public Animal Reproduce(Animal partner)
+        {
+
+        }
+    }
+}
